27001451da1922ceeba494a254a4c940a7198735
Rvlis <1065013799@qq.com>
2022-04-12 15:50:54 +0800

    update

--- OIE/src/run.py
+++ OIE/src/run.py
@@ -16,7 +16,7 @@ from C2S_part import text_simplify
 from NER_part import corenlp_chunk_candidate_relations_triples
 
 
-def single_extraction(content):
+def single_extraction(args, content):
     """
     单句抽取
     """
@@ -33,7 +33,7 @@ def single_extraction(content):
     for triple in candidate_relation_triples:
         print("{} --> ({}; {}; {})".format(triple[1], triple[3], triple[2], triple[4]))
 
-def batch_extraction(input_path):
+def batch_extraction(args, input_path):
     """
     批量抽取
     input_path: 指定输入文件路径
@@ -61,48 +61,30 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--coref", type=int, default=1, choices=[0,1], help="Whether turn on coreference resolution or not")
     parser.add_argument("-s", "--simp", type=int, default=1, choices=[0,1], help="Whether turn on c2s or not")
+    parser.add_argument("-t", "--type", type=int, default=0, choices=[0,1], help="选择抽取类型：单句抽取（0），批量抽取（1）；默认单句抽取")
     args = parser.parse_args()
 
     # 单句抽取
-    content = "Bell, a telecommunication company, which is based in Los Angeles"
-    single_extraction(content)
-
+    if args.type == 0:
+        content = "Bell, a telecommunication company, which is based in Los Angeles"
+        single_extraction(args, content)
     # 批量抽取
-    # candidate_relation_triples = batch_extraction("../data/benchie-30.txt")
-
+    else:
+        candidate_relation_triples = batch_extraction(args, "../../CaRB/data/dev.txt")
+        # candidate_relation_triples = batch_extraction("../../CaRB/data/test.txt")
 
     # step 4): 性能评估
     # CaRB benchmark
-    # with open("../data/CaRB_output.txt", "w", encoding="utf-8") as wf:
-    #     for triple in candidate_relation_triples:
-    #         tmp_triple = list()
-    #         # note: CaRB Tab Seperated: sent, prob, pred, arg1, arg2, add prob to triples
-    #         tmp_triple.insert(0, triple[1].strip())
-    #         tmp_triple.insert(1, "1.0")
-    #         tmp_triple.insert(2, triple[2].strip())
-    #         tmp_triple.insert(3, triple[3].strip())
-    #         tmp_triple.insert(4, triple[4].strip())
-    #         wf.write("\t".join(tmp_triple)+"\n")
+    with open("../data/CaRB_output_dev.txt", "w", encoding="utf-8") as wf:
+    # with open("../data/CaRB_output_test.txt", "w", encoding="utf-8") as wf:
+        for triple in candidate_relation_triples:
+            tmp_triple = list()
+            # note: CaRB Tab Seperated: sent, prob, pred, arg1, arg2, add prob to triples
+            tmp_triple.insert(0, triple[1].strip())
 
-    # Benchie benchmark
-    # with open("../data/benchie_30_output.txt", "w", encoding="utf-8") as wf:
-    #     for triple in candidate_relation_triples:
-    #         # triple: list, [sent_id, raw_sentence, VP, NP1, NP2]
-    #         triple[0], triple[1] = triple[1], triple[0]
-    #         triple[2], triple[3] = triple[3], triple[2]
-    #         triple[1] = str(triple[1])
-
-    #         wf.write("\t".join(triple[1:])+"\n")
-    
-    # os.system("pause")
-    # atomic proposition with the position of head and tail: [sentence, [start, end], [start, end]]
-    # atomic_propositions = list()
-    # with open("../data/atomic_propositions.txt", "w", encoding="utf-8") as wf:    
-    #     for relation_triple in candidate_relation_triples:
-    #         h_start = 0
-    #         h_end = len(relation_triple[0])
-    #         t_start = len(relation_triple[0])+1 + len(relation_triple[1])+1
-    #         t_end = len(relation_triple[0])+1 + len(relation_triple[1])+1 + len(relation_triple[2])
-    #         atomic_proposition = [str(relation_triple[0] + " " + relation_triple[1] + " " + relation_triple[2]), [h_start, h_end], [t_start, t_end]]
-    #         # print(atomic_proposition) 
-    #         wf.write(str(relation_triple[0] + " " + relation_triple[1] + " " + relation_triple[2]) + "\t" + str(h_start) + "\t" + str(h_end) + "\t" + str(t_start) + "\t" + str(t_end) + "\n")
+            # 因为本方法没有设置置信度，此处设置为定值1.0即可，不影响评估效果
+            tmp_triple.insert(1, "1.0")
+            tmp_triple.insert(2, triple[2].strip())
+            tmp_triple.insert(3, triple[3].strip())
+            tmp_triple.insert(4, triple[4].strip())
+            wf.write("\t".join(tmp_triple)+"\n")
